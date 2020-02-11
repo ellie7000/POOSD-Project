0ae7679f9aaa8681cffed079562e5a38c4453570
Name <44620962+ellie7000@users.noreply.github.com>
2020-02-10 22:08:32 -0500

    specified type any to be type express request/response in majors and courses apis

--- server/src/controllers/courses.ts
+++ server/src/controllers/courses.ts
@@ -1,13 +1,14 @@
 import Database from '../db';
+import Express from 'express';
 
 export module Courses {
-    export const getAllCourses = async (req: any, res: any) => {
+    export const getAllCourses = async (req: Express.Request, res: Express.Response) => {
         await Database.connectToMongo();
         res.type("json");
         Database.classes.find({}).toArray().then((result) => res.send(JSON.stringify(result)));
     };
 
-    export const getCourse = async (req: any, res: any) => {
+    export const getCourse = async (req: Express.Request, res: Express.Response) => {
         await Database.connectToMongo();
         res.type("json");
         Database.classes.findOne({ _id: Database.makeId(req.params.id) }).then((result) => res.send(JSON.stringify(result)));
--- server/src/controllers/majors.ts
+++ server/src/controllers/majors.ts
@@ -1,13 +1,14 @@
 import Database from '../db';
+import Express from 'express';
 
 export module Majors {
-    export const getAllMajors = async (req: any, res: any) => {
+    export const getAllMajors = async (req: Express.Request, res: Express.Response) => {
         await Database.connectToMongo();
         res.type("json");
         Database.majors.find({}).toArray().then((result) => res.send(JSON.stringify(result)));
     };
 
-    export const getMajor = async (req: any, res: any) => {
+    export const getMajor = async (req: Express.Request, res: Express.Response) => {
         await Database.connectToMongo();
         res.type("json");
         Database.majors.findOne({ _id: Database.makeId(req.params.id) }).then((result) => res.send(JSON.stringify(result)));
