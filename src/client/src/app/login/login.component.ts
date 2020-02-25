a9dd0581d59d462196b6c479fb1b6a1f8390aa80
ellie7000 <44620962+ellie7000@users.noreply.github.com>
2020-02-24 20:21:24 -0500

    updated register form

--- src/client/src/app/login/login.component.ts
+++ src/client/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
 
-@Component({ templateUrl: 'login.component.html' })
+@Component({ 
+    templateUrl: 'login.component.html',
+    styleUrls: ['./login.component.css']
+ })
 export class LoginComponent { }
