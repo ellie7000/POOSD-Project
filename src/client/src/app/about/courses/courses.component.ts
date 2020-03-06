f1cfafad9097c2e5c7b9adf52b0eeba587169cb9
ellie7000 <44620962+ellie7000@users.noreply.github.com>
2020-03-06 18:59:39 -0500

    created search bar and select list for courses taken

--- src/client/src/app/about/courses/courses.component.ts
+++ src/client/src/app/about/courses/courses.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { MDBModalRef } from 'angular-bootstrap-md';
+import { Course } from 'src/app/models/course.model';
+import { CoursesService } from 'src/app/services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
+
 export class CoursesComponent implements OnInit {
 
-  constructor(public modalRef: MDBModalRef) { }
+  courses: Course[] = [];
 
-  ngOnInit() {
+  constructor(public modalRef: MDBModalRef, private courseService: CoursesService) { }
+
+  async ngOnInit() {
+    await this.getCourses();
   }
+
+  async getCourses() {
+    this.courses = await this.courseService._coursesPromise;
+  }
+
 
 }
