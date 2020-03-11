d2858d8094f2bc56ff2981f0186ed23d26b10ac4
ellie7000 <44620962+ellie7000@users.noreply.github.com>
2020-03-11 16:23:50 -0400

    hooked up register page to backend server

--- src/client/src/app/register/register.component.ts
+++ src/client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  firstName: string;
+  lastName: string;
+  email: string; 
+  username: string; 
+  password: string;
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  async onSave() {
+    if (this.validate()) {
+      const data = await this.userService.register(this.firstName, this.lastName, this.email, this.username, this.password);
+      console.log("here");
+      console.log(data);
+      this.router.navigateByUrl('/login');
+    }
+    else {
+
+    }
+  }
+
+  validate(): boolean {
+    let valid: boolean = true;
+
+    if (this.isEmptyOrSpaces(this.username)) {
+      valid = false;
+    }
+    if (this.isEmptyOrSpaces(this.password)) {
+      valid = false;
+    }
+
+    // Check rest of attributes
+
+    return valid;
+  }
+
+  isEmptyOrSpaces(str: string) {
+    return str === null || str.match(/^ *$/) !== null || str.length == 0;
   }
 
 }
